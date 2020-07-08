ebc2658e424870e2bba3056b40ab069024f5963e
ArtyomZayarny <artyom.zayarny@gmail.com>
2020-07-08 07:52:37 +0300

    Updates

---
+++ precache-manifest.599a632362edf6c5af40e1d88bd5c5f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccfb832d25a607ff66cf1fa3eee08a9b",
+    "url": "/mini-blog/index.html"
+  },
+  {
+    "revision": "f466d09668fb7633778d",
+    "url": "/mini-blog/static/css/main.4553adda.chunk.css"
+  },
+  {
+    "revision": "555719e4156d718a913c",
+    "url": "/mini-blog/static/js/2.b376b646.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mini-blog/static/js/2.b376b646.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f466d09668fb7633778d",
+    "url": "/mini-blog/static/js/main.1795a6d7.chunk.js"
+  },
+  {
+    "revision": "e2c1087c9a84cfb7fc49",
+    "url": "/mini-blog/static/js/runtime-main.3f360af4.js"
+  }
+]);
